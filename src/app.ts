4db95bd71baf479281fae76bb533b465762071d3
조준서 <jun.joy@cenacle.com>
2023-04-07 14:02:55 +0900

    chore: add console

--- src/app.ts
+++ src/app.ts
@@ -11,24 +11,30 @@ async function main() {
     dotenv.config();
 
     // 평일 오전 10시 실행
-    const crawlingCycle = "0 10 * * 1-5";
+    const crawlingCycle = "0 9 * * 1-5";
 
     cron.schedule(crawlingCycle, async () => {
-        const prevBlogData = await getBlogDatas(firestore);
-
-        const withRSSBlogData = await crawler.getTechBlogDataWithRSS();
-        const withoutRSSBlogData = await crawler.getTechBlogDataWithoutRSS();
-
-        const currentBlogData = [...withRSSBlogData, ...withoutRSSBlogData];
-
-        const newFeeds = getNewFeedDatas(prevBlogData, currentBlogData) ?? [];
-
-        if (newFeeds.length <= 0) return;
-
-        await sendGreetings();
-        await sendSlackMessage(newFeeds);
-
-        await postBlogDatas(firestore, currentBlogData);
+        try {
+            console.log("크롤링 시작");
+            const prevBlogData = await getBlogDatas(firestore);
+            
+            const withRSSBlogData = await crawler.getTechBlogDataWithRSS();
+            const withoutRSSBlogData = await crawler.getTechBlogDataWithoutRSS();
+            
+            const currentBlogData = [...withRSSBlogData, ...withoutRSSBlogData];
+            
+            const newFeeds = getNewFeedDatas(prevBlogData, currentBlogData) ?? [];
+            
+            if (newFeeds.length <= 0) return;
+            
+            await sendGreetings();
+            await sendSlackMessage(newFeeds);
+            
+            await postBlogDatas(firestore, currentBlogData);
+            console.log('크롤링 완료')
+        } catch (error) {
+            console.log('에러가 발생하였습니다.', error);
+        }
     });
 }
 
